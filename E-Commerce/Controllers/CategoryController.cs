a983db87a4fc8ae549d160606e9b558cadb20acc
Amirhanna2001 <amirhanna111200@gmail.com>
2023-02-25 11:41:35 +0200

    Category Alter Index View

---
+++ E-Commerce/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using E_Commerce.Data;
+using E_Commerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task< IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if(!ModelState.IsValid)
+                return View(category);
+
+            if(category == null)
+                return View("Error");
+
+            if(await _context.Categories.AnyAsync(c => c.Name == category.Name))
+            {
+                ModelState.AddModelError("Name", "This Category Is Exists");
+                return View(category);
+            }
+            
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
