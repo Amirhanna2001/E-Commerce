ea0695e2eaf896c2f2f7475817504bee66230b57
Amirhanna2001 <amirhanna111200@gmail.com>
2023-02-25 11:02:07 +0200

    Category Index

---
+++ E-Commerce/Controllers/Category.cs
@@ -0,0 +1,19 @@
+﻿using E_Commerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Controllers
+{
+    public class Category : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public Category(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task< IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+    }
+}
--- E-Commerce/Controllers/HomeController.cs
+++ E-Commerce/Controllers/HomeController.cs
@@ -28,5 +28,9 @@ namespace E_Commerce.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
     }
 }
